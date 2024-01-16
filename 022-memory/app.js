edbfcc1b29344a06c5e9aecae74265e773ba0693
Aleksandar Ristic <storiaca@gmail.com>
2024-01-16 18:22:03 +0100

    Memory finished

--- 022-memory/app.js
+++ 022-memory/app.js
@@ -1,3 +1,6 @@
+// ovo je bitna variabla
+let level = 1;
+
 let icons = [
   "A",
   "S",
@@ -36,28 +39,37 @@ let icons = [
   "Q",
   "W",
 ];
-let container = document.querySelector(".container");
 
+let wins = 0;
+
+let twoFlipped = [];
+let container = document.querySelector(".container");
 
 createGrid();
 let allCards = document.querySelectorAll(".card");
 
-allCards.forEach(card => card.addEventListener("click", flipCard))
+allCards.forEach((card) => card.addEventListener("click", flipCard));
 
 function flipCard() {
+  this.removeEventListener("click", flipCard);
   // let front = this.querySelector('.front');
   // let back = this.querySelector('.back');
 
   // front.style.transform = "perspective(900px) rotateY(180deg)";
   // back.style.transform = "perspective(900px) rotateY(0)";
+  twoFlipped.push(this);
 
-  this.className = "card active"
+  this.classList.add("active");
+
+  if (twoFlipped.length === 2) {
+    checkCards();
+  }
 }
 
 function createGrid() {
   let cardsHtml = "";
   for (let i = 0; i < 36; i++) {
-    let rand = Math.floor(Math.random() * icons.length)
+    let rand = Math.floor(Math.random() * icons.length);
     cardsHtml += `
         <div class="card">
             <div class="back">${icons[rand]}</div>
@@ -68,4 +80,44 @@ function createGrid() {
   }
   container.innerHTML = cardsHtml;
 }
-// 28:36
+
+function checkCards() {
+  removeAllClicks();
+  let back1 = twoFlipped[0].querySelector(".back");
+  let back2 = twoFlipped[1].querySelector(".back");
+  console.log(back1.innerHTML, back2.innerHTML);
+  if (back1.innerHTML === back2.innerHTML) {
+    // pogodak
+    wins++;
+    if (wins === 18) {
+      // NEXT LEVEL Level2()
+      level++;
+      game();
+    }
+    twoFlipped = [];
+    returnClicks();
+  } else {
+    setTimeout(() => {
+      twoFlipped[0].classList.remove("active");
+      twoFlipped[1].classList.remove("active");
+      twoFlipped.length = 0;
+      returnClicks();
+    }, 700);
+  }
+}
+
+function removeAllClicks() {
+  allCards.forEach((card) => card.removeEventListener("click", flipCard));
+}
+
+function returnClicks() {
+  // allCards.forEach((card) => {
+  //   if (!card.classList.contains("active")) {
+  //     card.addEventListener("click", flipCard);
+  //   }
+  // });
+
+  let cardsNoActive = document.querySelectorAll(".card:not(.active)");
+  cardsNoActive.forEach((card) => card.addEventListener("click", flipCard));
+}
+//
