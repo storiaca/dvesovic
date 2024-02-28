c5067ce69ed99607f55a4dbc408234ce85408038
Aleksandar Ristic <storiaca@gmail.com>
2024-02-28 17:56:55 +0100

    Create interval

--- 023-dice/app.js
+++ 023-dice/app.js
@@ -1,5 +1,21 @@
-const titleStart = document.getElementById("title");
+const titleStart = document.getElementById("start");
+const player1Dice = document.querySelector(".player1-dice");
+const player2Dice = document.querySelector(".player2-dice");
 
 titleStart.addEventListener("click", startGame);
 
-function startGame() {}
+function startGame() {
+  titleStart.removeEventListener("click", startGame);
+  let counter = 5;
+  const counterId = setInterval(() => {
+    titleStart.innerHTML = counter;
+    counter--;
+    if (counter === 0) {
+      clearInterval(counterId);
+      titleStart.innerHTML = "Player 1 turn";
+    }
+  }, 1000);
+}
+
+console.log(player1Dice, player2Dice);
+//
