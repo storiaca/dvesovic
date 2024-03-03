ad88acfa4d19f4723c18d4a7748f274dc0dbce1e
Aleksandar Ristic <storiaca@gmail.com>
2024-03-03 22:39:18 +0100

    Get values from form for new phone

--- 024-spa-1/app.js
+++ 024-spa-1/app.js
@@ -1,11 +1,64 @@
 // views
 let allPhoneViews = document.getElementById("all-phones-view");
+let addView = document.getElementById("add-view");
+
+// inputs
+let idInput = document.querySelector('input[name="id"');
+let brandInput = document.querySelector('input[name="brand"');
+let nameInput = document.querySelector('input[name="name"');
+let priceInput = document.querySelector('input[name="price"');
+let availableInput = document.querySelector('select[name="available"');
 
 // all phones tbody
 const allPhonesTbody = allPhoneViews.querySelector("tbody");
 
+// buttons
+const allBtn = document.getElementById("allBtn");
+const addBtn = document.getElementById("addBtn");
+const editBtn = document.getElementById("editBtn");
+const saveBtn = document.getElementById("saveBtn");
+
+// listeners
+allBtn.addEventListener("click", displayAllPhonesView);
+addBtn.addEventListener("click", displayAddView);
+saveBtn.addEventListener("click", saveNewPhone);
+
 // start app
 createMainTable();
+
+// functions
+function displayAddView() {
+  allPhoneViews.style.display = "none";
+  addView.style.display = "block";
+}
+
+function displayAllPhonesView() {
+  addView.style.display = "none";
+  allPhoneViews.style.display = "block";
+}
+
+function saveNewPhone() {
+  let newPhone = {
+    id: idInput.value,
+    brand: brandInput.value,
+    name: nameInput.value,
+    price: priceInput.value,
+    available: checkAvailable(availableInput.value),
+  };
+
+  db.push(newPhone);
+  createMainTable();
+  displayAllPhonesView();
+}
+
+function checkAvailable(val) {
+  // if (val === "true") {
+  //   return true
+  // } else {
+  //   return false
+  // }
+  return val === "true" ? true : false;
+}
 
 function createMainTable() {
   let phonesHtml = "";
