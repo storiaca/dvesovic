33f58dcd771096eeb042c099a7c427ca79508355
Aleksandar Ristic <storiaca@gmail.com>
2024-03-03 21:10:17 +0100

    Add data in table via javascript

--- 024-spa-1/app.js
+++ 024-spa-1/app.js
@@ -1,1 +1,46 @@
-// 13:58
+// views
+let allPhoneViews = document.getElementById("all-phones-view");
+
+// all phones tbody
+const allPhonesTbody = allPhoneViews.querySelector("tbody");
+
+// start app
+createMainTable();
+
+function createMainTable() {
+  let phonesHtml = "";
+
+  // for (let i = 0; i < db.length; i++) {
+  //   phonesHtml += `
+  //       <tr>
+  //         <td>${db[i].id}</td>
+  //         <td>${db[i].brand}</td>
+  //         <td>${db[i].name}</td>
+  //         <td>${db[i].price}</td>
+  //         <td>
+  //           <button class="btn btn-sm btn-${
+  //             db[i].available ? "success" : "danger"
+  //           }">${db[i].available === true ? "Yes" : "No"}</button>
+  //         </td>
+  //     </tr>
+  //     `.trim();
+  // }
+
+  db.map((phone) => {
+    return (phonesHtml += `
+      <tr>
+        <td>${phone.id}</td>
+        <td>${phone.brand}</td>
+        <td>${phone.name}</td>
+        <td>${phone.price}</td>
+        <td>
+          <button class="btn btn-sm btn-${
+            phone.available ? "success" : "danger"
+          }">${phone.available ? "Yes" : "No"}</button>
+        </td>
+    </tr>
+  `.trim());
+  });
+  allPhonesTbody.innerHTML = phonesHtml;
+}
+console.log(db);
---
+++ 024-spa-1/database.js
@@ -0,0 +1,23 @@
+let db = [
+  {
+    id: "1",
+    brand: "Samsung",
+    name: "Note 10",
+    price: 12000,
+    available: false,
+  },
+  {
+    id: "2",
+    brand: "iPhone",
+    name: "12 Pro Max",
+    price: 19000,
+    available: true,
+  },
+  {
+    id: "3",
+    brand: "Redmi",
+    name: "Pro Note 10",
+    price: 6000,
+    available: true,
+  },
+];
