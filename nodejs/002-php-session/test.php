38207e5fab561dca8ac71ac4157b2607c9fe4168
Aleksandar Ristic <storiaca@gmail.com>
2024-05-12 12:31:24 +0200

    Php test variables

---
+++ nodejs/002-php-session/test.php
@@ -0,0 +1,17 @@
+<?php 
+    $name = 'Aleksandar';
+    // var_dump($name)
+    $num = 34;
+    $nista = null;
+    $istina = true;
+
+    $names = ['Ana', 'Marija'];
+    $person = [
+        "name" => "Aleksandar",
+        "age" => 42
+    ];
+
+    echo $names[0];
+    echo $person['name'];
+
+?>
