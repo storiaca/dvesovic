e70ca135485979990b89e19222f18d7b54ee519b
Aleksandar Ristic <storiaca@gmail.com>
2024-05-20 14:15:55 +0200

    Server setup

--- nodejs/006-http-server/server/server.js
+++ nodejs/006-http-server/server/server.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+// const dayjs = require("dayjs");
+
+// const now = new Date();
+
+// console.log(dayjs(now).format("DD/MM/YYYY"));
+
+const http = require("http");
+// const net = require("net");
+
+const server = http.createServer(function (req, res) {
+  // console.log("Neki Japanac trazi nesto");
+  res.write("<h1>Sacekaj malo Japanac</h1>");
+  res.end();
+});
+
+server.listen(5000, function () {
+  console.log("Pokrenuo se server");
+});
