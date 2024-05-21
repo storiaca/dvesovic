ed1d1631b4c7829cf498f46bdeaa861ecba64822
Aleksandar Ristic <storiaca@gmail.com>
2024-05-21 09:15:48 +0200

    Konsultacije zavrseno

--- nodejs/007-konsultacije/server.js
+++ nodejs/007-konsultacije/server.js
@@ -1,10 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   // logika????
-  res.end("Hello");
+  // if (req.url === "/users") {
+  //   res.end(JSON.stringify({ name: "Aleksandar" }));
+  // }
+
+  const name = "Aleksandar";
+  fs.readFile("./index.html", "utf-8", (err, file) => {
+    console.log(file);
+    let fileExport = file.replace("{{name}}", name);
+    res.end(fileExport);
+  });
 });
 
 server.listen(8080, function () {
   console.log("Listening on port 8080");
 });
-console.log("hello");
