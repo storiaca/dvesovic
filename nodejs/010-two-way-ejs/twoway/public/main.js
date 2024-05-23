eb15c960d3c4d3a89e1e185d75f2c9c72a1548d1
Aleksandar Ristic <storiaca@gmail.com>
2024-05-23 20:04:54 +0200

    Homework finished

--- nodejs/010-two-way-ejs/twoway/public/main.js
+++ nodejs/010-two-way-ejs/twoway/public/main.js
@@ -6,48 +6,60 @@ let allMessages = document.querySelector("#all-messages");
 let latestMessages = [];
 let html = ``;
 
+window.addEventListener("load", () => {
+  fetch("/messages")
+    .then((res) => res.json())
+    .then((data) => showData(data.data));
+});
+
 subBtn.addEventListener("click", function (e) {
   e.preventDefault();
 
-  // fetch("/messages", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({
-  //     email: emailInput.value,
-  //     message: messageInput.value,
-  //   }),
-  // })
-  //   .then((res) => res.json())
-  //   .then((msg) => {
-  //     latestMessages = msg.data;
-  //     if (latestMessages.length > 1) {
-  //       latestMessages.forEach((latest) => {
-  //         const formattedDate = new Date(latest.date).toLocaleDateString(
-  //           "en-US",
-  //           {
-  //             year: "numeric",
-  //             month: "short",
-  //             day: "numeric",
-  //             hour: "2-digit",
-  //             minute: "2-digit",
-  //           }
-  //         );
-  //         allMessages.innerHTML += `
-  //         <dl>
-  //           <dt>${latest.email}</dt>
-  //           <dd>${latest.message}</dd>
-  //           <dt>${formattedDate}</dt>
-  //         </dl>
-  //         `;
-  //       });
-  //     }
-  //   })
-  //   .catch((err) => console.log(err));
+  fetch("/messages", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: emailInput.value,
+      message: messageInput.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((msg) => {
+      showData(msg.data);
+    })
+    .catch((err) => console.log(err));
 
-  let xml = new XMLHttpRequest();
+  // let xml = new XMLHttpRequest();
+
+  // xml.open("/messages");
+
+  // xml.send();
 
   emailInput.value = "";
   messageInput.value = "";
 });
+
+function showData(messages) {
+  if (messages.length > 1) {
+    html = "";
+    messages.forEach((latest) => {
+      const formattedDate = new Date(latest.date).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+      html += `
+        <dl>
+          <dt>${latest.email}</dt>
+          <dd>${latest.message}</dd>
+          <dt>${formattedDate}</dt>
+        </dl>
+        `;
+    });
+    allMessages.innerHTML = html;
+  }
+}
--- nodejs/010-two-way-ejs/twoway/server.js
+++ nodejs/010-two-way-ejs/twoway/server.js
@@ -8,20 +8,26 @@ app.use(express.static(__dirname + "/node_modules/bootstrap/dist/css/"));
 //app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.get("/", function (req, res) {
-//   fs.readFile("./messages.json", "utf-8", (err, content) => {
-//     let arr = JSON.parse(content);
-//     fs.writeFile("./messages.json", JSON.stringify(arr), (err) => {
-//       res.send(JSON.stringify({ status: "super", data: arr }));
-//     });
-//   });
-// });
+app.get("/messages", function (req, res) {
+  fs.readFile("./messages.json", "utf-8", (err, content) => {
+    let arr = JSON.parse(content);
+    arr.sort((a, b) => b.timestamp - a.timestamp);
+    arr.length = 5;
+    res.send(JSON.stringify({ status: "super", data: arr }));
+  });
+});
 
 app.post("/messages", (req, res) => {
   fs.readFile("./messages.json", "utf-8", (err, content) => {
     let arr = JSON.parse(content);
-    req.body.date = new Date();
-    arr.push(req.body);
+    // req.body.date = new Date();
+    arr.push({
+      ...req.body,
+      date: new Date(),
+      timestamp: new Date().getTime(),
+    });
+    arr.sort((a, b) => b.timestamp - a.timestamp);
+    arr.length = 5;
     fs.writeFile("./messages.json", JSON.stringify(arr), (err) => {
       res.send(JSON.stringify({ status: "super", data: arr }));
     });
