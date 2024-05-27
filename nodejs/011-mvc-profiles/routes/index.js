6f925218ceef9d91ed479c727c5c476e9f5a58eb
Aleksandar Ristic <storiaca@gmail.com>
2024-05-27 09:04:15 +0200

    Profile table and dropdown

--- nodejs/011-mvc-profiles/routes/index.js
+++ nodejs/011-mvc-profiles/routes/index.js
@@ -1,1 +1,12 @@
-// 25:29
+const express = require("express");
+const fs = require("fs");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  fs.readFile("./data.json", "utf-8", (err, file) => {
+    res.render("index", { accounts: JSON.parse(file) });
+  });
+});
+
+module.exports = router;
--- nodejs/011-mvc-profiles/server.js
+++ nodejs/011-mvc-profiles/server.js
@@ -1,15 +1,14 @@
 const express = require("express");
-
+const routes = require("./routes");
 const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+app.use(express.static(__dirname + "/node_modules/bootstrap/dist"));
 app.use(express.static(__dirname + "/public"));
-app.get("/", (req, res) => {
-  res.render("index");
-});
 
+// root
+app.use("/", routes);
 app.listen(8080, function () {
   console.log("Listening on 8080 port");
 });
