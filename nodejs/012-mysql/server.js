ca7834a724368a75da10f6e11afcdc11ae08a752
Aleksandar Ristic <storiaca@gmail.com>
2024-05-29 09:01:05 +0200

    Connect to db with async/await

--- nodejs/012-mysql/server.js
+++ nodejs/012-mysql/server.js
@@ -7,7 +7,7 @@ const db = mysql2
     host: "localhost",
     user: "root",
     password: process.env.DB_PASSWORD,
-    database: "bootjs",
+    database: "boot",
   })
   .promise();
 
@@ -17,7 +17,12 @@ app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const [data] = await db.query("SELECT * FROM profiles");
+  console.log(data);
+
+  res.send("ok");
+
   // db.query("SELECT * FROM profiles", (err, data) => {
   //   console.log(data);
   //   res.send("Ok");
