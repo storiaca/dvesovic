ee35aea90c2cc28b6693aa0a885e9ca86d6dbb4d
Aleksandar Ristic <storiaca@gmail.com>
2024-06-30 13:39:44 +0200

    Register user

--- social-app/backend/controllers/auth/register.js
+++ social-app/backend/controllers/auth/register.js
@@ -1,7 +1,28 @@
+const UserModel = require("../../model/UserModel");
+const bcrypt = require("bcryptjs");
+const SALT = process.env.SALT;
+
 const register = async (req, res) => {
-  console.log(req.body);
+  const { email, password, confirmPassword } = req.body;
   try {
-  } catch (error) {}
+    const checkUser = await UserModel.findOne({ email });
+    if (checkUser) {
+      res.status(201).send({ msg: "User already exist" });
+    } else {
+      if (password === confirmPassword) {
+        const hashPassword = await bcrypt.hash(password, parseInt(SALT, 10));
+        const newUser = new UserModel({ ...req.body, password: hashPassword });
+        const saveUser = await newUser.save();
+        res.send(saveUser);
+      } else {
+        res
+          .status(201)
+          .send({ msg: "Password and confirm password not match" });
+      }
+    }
+  } catch (error) {
+    res.status(415).send({ msg: error.message });
+  }
 };
 
 module.exports = register;
